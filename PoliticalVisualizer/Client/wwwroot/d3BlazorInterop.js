3b7b3f67eb5ef023e5cb35e97f410bf381517d59
AlexKven <kvenclassical@gmail.com>
2020-07-29 20:36:36 -0700

    Trying to get map to work.

---
+++ PoliticalVisualizer/Client/wwwroot/d3BlazorInterop.js
@@ -0,0 +1,16 @@
+﻿window.d3Blazor = {
+    test: function () {
+        var map = d3.choropleth()
+            .geofile('https://d3-geomap.github.io/d3-geomap/topojson/countries/USA.json')
+            .projection(d3.geoAlbersUsa)
+            .column('2012')
+            .unitId('fips')
+            .scale(1000)
+            .legend(true);
+
+        d3.csv('https://d3-geomap.github.io/data/venture-capital.csv').then(data => {
+            map.draw(d3.select('#map').datum(data));
+        });
+        //map.draw(d3.select('#map'));
+    }
+};
